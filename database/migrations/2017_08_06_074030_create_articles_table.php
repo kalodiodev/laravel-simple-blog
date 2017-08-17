0c4f8927b881b50d9f38a158c300722279a6c96a
Athanasios Raptodimos <thanosra@gmail.com>
2017-08-17 20:45:27 +0300

    Set article slug unique constraint

--- database/migrations/2017_08_06_074030_create_articles_table.php
+++ database/migrations/2017_08_06_074030_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->string('description');
             $table->string('keywords')->nullable();
             $table->text('body');
