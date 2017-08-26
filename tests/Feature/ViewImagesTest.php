f23b0ca4801a4b9d72de563bcccefd473091fce2
Athanasios Raptodimos <thanosra@gmail.com>
2017-08-26 22:20:05 +0300

    Test avatar image view

--- tests/Feature/ViewImagesTest.php
+++ tests/Feature/ViewImagesTest.php
@@ -22,13 +22,36 @@ class ViewImagesTest extends TestCase
         
         $response->assertHeader('Content-Type', 'image/png');
     }
+    
+    /** @test */
+    public function a_user_can_view_an_avatar_image()
+    {
+        Storage::fake('testfs');
+
+        UploadedFile::fake()->image('avatar.png')
+            ->storeAs('images/avatar/', 'avatar.png');
+
+        $response = $this->get('/images/avatar/avatar.png')
+            ->assertStatus(200);
+
+        $response->assertHeader('Content-Type', 'image/png');
+    }
 
     /** @test */
-    public function should_respond_404_when_file_does_not_exist()
+    public function should_respond_404_when_featured_image_does_not_exist()
     {
         Storage::fake('testfs');
         
         $this->get('/images/featured/missing.png')
+            ->assertStatus(404);
+    }
+
+    /** @test */
+    public function should_respond_404_when_avatar_image_does_not_exist()
+    {
+        Storage::fake('testfs');
+
+        $this->get('/images/avatar/missing.png')
             ->assertStatus(404);
     }
 }
